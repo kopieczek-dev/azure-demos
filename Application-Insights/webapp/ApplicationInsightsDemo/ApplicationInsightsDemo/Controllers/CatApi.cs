964d5a614918dd6a43e1f1e308cc48e3e4d8531d
Krzysztof Kopieczek <krzysztof.kopieczek@chmurowisko.pl>
2023-12-18 12:14:44 +0100

    Changed concept to API app

---
+++ Application-Insights/webapp/ApplicationInsightsDemo/ApplicationInsightsDemo/Controllers/CatApi.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ApplicationInsightsDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CatApi : ControllerBase
+    {
+        private readonly ILogger<CatApi> _logger;
+
+        public CatApi(ILogger<CatApi> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var client = new HttpClient();
+
+            var resp = await client.GetAsync("https://api.thecatapi.com/v1/images/search");
+            var respString = await resp.Content.ReadAsStringAsync();
+
+            dynamic data = JsonConvert.DeserializeObject(respString)
+                ?? throw new ArgumentException(nameof(data));
+            var catImgUrl = data[0].url;
+
+            var imgResp = await client.GetAsync(catImgUrl.ToString());
+            var img = await imgResp.Content.ReadAsStreamAsync();
+
+            return new FileStreamResult(img, "image/jpeg");
+        }
+    }
+}
