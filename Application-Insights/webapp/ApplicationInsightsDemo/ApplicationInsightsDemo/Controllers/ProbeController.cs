a6ea6969be3943d3fcd88a3666c146fd73547c50
Krzysztof Kopieczek <krzysztof.kopieczek@chmurowisko.pl>
2023-12-18 14:26:49 +0100

    Added readiness and liveness probes

---
+++ Application-Insights/webapp/ApplicationInsightsDemo/ApplicationInsightsDemo/Controllers/ProbeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace ApplicationInsightsDemo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProbeController : ControllerBase
+    {
+        [HttpGet("liveness")]
+        public IActionResult Liveness()
+        {
+            return Ok();
+        }
+
+        [HttpGet("readiness")]
+        public IActionResult Readiness()
+        {
+            var stopwatch = new System.Diagnostics.Stopwatch();
+            stopwatch.Start();
+
+            ExecuteDB();
+
+            stopwatch.Stop();
+            var executionTime = stopwatch.Elapsed;
+
+            if (executionTime > TimeSpan.FromMilliseconds(10))
+            {
+                throw new Exception("Database query took too long");
+            }
+
+            return Ok();
+        }
+
+        private static void ExecuteDB()
+        {
+            var connectionString =
+                Environment.GetEnvironmentVariable("AZURE_SQL_CONNECTIONSTRING")!;
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT @@VERSION";
+                SqlCommand command = new SqlCommand(query, connection);
+
+                var dbVer = (string)command.ExecuteScalar();
+            }
+        }
+    }
+}
