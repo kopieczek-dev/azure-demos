938eb11e0f0790d4f2185b83cd2bb83c64c15a06
Krzysztof Kopieczek <krzysztof.kopieczek@chmurowisko.pl>
2023-12-18 14:06:41 +0100

    Added database

---
+++ Application-Insights/webapp/ApplicationInsightsDemo/ApplicationInsightsDemo/Controllers/DatabaseController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace ApplicationInsightsDemo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class DatabaseController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<DatabaseController> _logger;
+
+        public DatabaseController(IConfiguration configuration, ILogger<DatabaseController> logger)
+        {
+            _configuration = configuration;
+            this._logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult GetCurrentNumberOfOrders()
+        {
+            int numberOfOrders = 0;
+
+            var connectionString = 
+                Environment.GetEnvironmentVariable("AZURE_SQL_CONNECTIONSTRING")!;
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "select count(*) from [SalesLT].[SalesOrderHeader]";
+                SqlCommand command = new SqlCommand(query, connection);
+
+                numberOfOrders = (int)command.ExecuteScalar();
+            }
+
+            return Ok(numberOfOrders);
+        }
+    }
+}
