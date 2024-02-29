cdf6ed90f73b77583462703691cd604308bb2d49
Krzysztof Kopieczek <krzysztof.kopieczek@chmurowisko.pl>
2024-02-29 09:40:02 +0100

    Added App Service Web App demo

---
+++ App-Service/WebAppDemo/WebAppDemo/Pages/Settings.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAppDemo.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+        private readonly IConfiguration _configuration;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public void OnGet()
+        {
+            ViewData["Setting"] = _configuration["Demo:Setting"];
+        }
+    }
+
+}
